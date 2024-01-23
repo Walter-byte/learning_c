5802537074e00f687c2e67c90ba40fc767042556
Walter-byte <sallar.ebadi@gmail.com>
2024-01-23 18:19:24 +0330

    new file:   Learning C++/7-getch.cpp 	new file:   Learning C++/output/7-getch

---
+++ Learning C++/7-getch.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char x;
+    x = getchar();      //getch in the book !!
+    cout << x;
+
+    return 0;
+}
